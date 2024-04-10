e00e9003954b0621dfa3cf4ff65956df846cd225
Abhimanyu <abhimanyu19072002@gmail.com>
2024-04-11 03:08:16 +0530

    google login setup

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,26 +3,32 @@ import "../styles/Login.scss";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../context/globalContext";
 
-
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+  GithubLoginButton,
+} from "react-social-login-buttons";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { login } = useGlobalContext(); 
+  const { login } = useGlobalContext();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      await login({ email, password }); 
+      await login({ email, password });
       navigate("/");
     } catch (err) {
       console.log("Login failed : ", err.message);
     }
   };
-
+  const handleClick = () => {
+    alert("React Social Login Buttons!");
+  };
   return (
     <div className="login">
       <div className="login_content">
@@ -43,6 +49,12 @@ const LoginPage = () => {
           />
           <button type="submit">LOG IN</button>
         </form>
+        <div>
+          {"or"}
+          <GoogleLoginButton onClick={handleClick}/>
+          {/*<FacebookLoginButton onClick={handleClick}/>
+          <GithubLoginButton onClick={handleClick}/>*/}
+        </div>
         <a href="/register">Don't have an account? Sign In Here</a>
       </div>
     </div>
