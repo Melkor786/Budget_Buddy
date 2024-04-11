2b9b5d67f3281b5fa7b2b0ec1cb6c8eac20d189e
Abhimanyu <abhimanyu19072002@gmail.com>
2024-04-11 14:22:04 +0530

    updated globalcontext

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1/transaction";
+const BASE_URL = "http://localhost:5000/api/v1/";
 
 const GlobalContext = React.createContext();
 
