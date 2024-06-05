3c13d74bca86dba5d301c537eb17106029f4b969
Abhimanyu Yadav <97447229+Melkor786@users.noreply.github.com>
2024-06-05 14:01:02 +0530

    Create transactionRoutes.js

---
+++ Backend/routes/transactionRoutes.js
@@ -0,0 +1,33 @@
+const {
+  addExpense,
+  getExpenses,
+  deleteExpense,
+} = require("../controllers/expenseController");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/incomeController");
+const {
+  addBill,
+  getBills,
+  deleteBill,
+} = require("../controllers/billController");
+const requireAuth = require("../middleware/requireAuth");
+
+const router = require("express").Router();
+
+// require Auth for all transaction methods
+router.use(requireAuth)
+
+router.post("/add-income", addIncome);
+router.get("/get-incomes", getIncomes);
+router.delete("/delete-income/:id", deleteIncome);
+router.post("/add-expense", addExpense);
+router.get("/get-expenses", getExpenses);
+router.delete("/delete-expense/:id", deleteExpense);
+router.post("/add-bill", addBill);
+router.get("/get-bills", getBills);
+router.delete("/delete-bill/:id", deleteBill);
+
+module.exports = router;
