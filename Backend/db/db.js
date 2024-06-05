ae49db7eb6aa45724d8c3bd0dca4829ddde722a4
Abhimanyu Yadav <97447229+Melkor786@users.noreply.github.com>
2024-06-05 14:06:30 +0530

    Create db.js

---
+++ Backend/db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try {
+        const options = {
+            dbName:"Ledger",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+        
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGO_URL, options);
+        console.log('Db Connected');
+    } catch (error) {
+        console.error('DB Connection Error:', error);
+    }
+};
+module.exports = { db };
