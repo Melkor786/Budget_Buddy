f1a6a0335890752483affbf50e326aa6af429803
Abhimanyu Yadav <97447229+Melkor786@users.noreply.github.com>
2024-11-16 12:31:49 +0530

    Create env.js

---
+++ src/api/env.js
@@ -0,0 +1,3 @@
+export const GOOGLE_CLIENT_ID = "605481735766-pm8bs9mclso29449j6dcq8vjlf7t69nl.apps.googleusercontent.com"
+// for localhost - export const BASE_URL = "http://localhost:5000/api/v1/"
+export const BASE_URL = "https://budget-buddy-6uti.onrender.com//api/v1/"
